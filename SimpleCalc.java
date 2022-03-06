fddd8bfb935e2504cf05da6916b5028803403484
SAYbaw <97255560+SAYbaw@users.noreply.github.com>
2022-03-06 17:22:55 -0500

    Add files via upload

---
+++ SimpleCalc.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class SimpleCalc {
+
+	public static void main(String[] args) {
+		
+		//call getImput() here
+		
+
+	}
+	
+	public static void getInput() {
+	
+		double num1 = 0.0;
+		double num2 = 0.0;
+		
+		//get user input here
+		
+		//num1, operation choice, num2
+		
+	}
+	
+	public static double addition (double n1, double n2) {
+		
+		return n1 + n1;
+	}
+	
+	public static void subtraction (){// change return value, add parameters
+		
+	}
+	
+	public static void multiply () {// change return value, add parameters
+		
+	}
+	
+	public static void divide () {// change return value, add parameters
+		//validate code to prevent divide by 0 in getInput()	
+	}
+	public static void percent () {// change return value, add parameters
+			
+	}
+	public static double power (double x, double y) {// change return value, add parameters
+		double answer = 1.0;
+		//hint use a for loop
+		
+		return answer;
+	}
+	
+	
+
+}
